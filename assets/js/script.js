00a99832cacc80f2396ddb098e6596184ff4c8d6
Kevin Eddie <keddie024@gmail.com>
2021-03-30 19:22:29 -0400

    completed first three functions in js // appears to be working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,87 @@
-var view = document.querySelector("#view-scores");
 var highScores = document.querySelector("#score-sheet");
+var startButton = document.querySelector("#start-button");
+var timer = document.querySelector("#timer");
+var questions = document.querySelector("#questions");
+var answers = document.querySelector("#answers");
 
-view.addEventListener("click", hide);
+var questionList = [
+    {
+        question: "Question1",
+        options: ["One", "Two", "Three", "Four"],
+        answer: "One"
+    },
+    {
+        question: "Question1",
+        options: ["One", "Two", "Three", "Four"],
+        answer: "One"
+    },
+    {
+        question: "Question1",
+        options: ["One", "Two", "Three", "Four"],
+        answer: "One"
+    },
+]
 
-function hide() {
-    highScores.removeAttribute("class", "hidden");
+var questionCount = 0;
+var timeLeft = questionList.length * 15;
+
+function startQuiz() {
+    var intro = document.querySelector("#intro");
+    intro.setAttribute("class", "hidden");
+    questions.removeAttribute("class");
+    questionCount = 0;
+    nextQuestion();
 }
+
+startButton.addEventListener("click", startQuiz);
+
+function nextQuestion() {
+    var currentQuestion = questionList[questionCount];
+    var question = document.querySelector("#question-text");
+    question.textContent = currentQuestion.question;
+
+    for (var i = 0; i < currentQuestion.options.length; i++) {
+        var optionButton = document.createElement("button");
+        optionButton.setAttribute("class", "choice");
+        optionButton.setAttribute("value", currentQuestion.options[i]);
+        optionButton.textContent = currentQuestion.options[i];
+        optionButton.addEventListener("click", checkAnswer);
+        answers.appendChild(optionButton);
+    }
+
+}
+
+function checkAnswer() {
+    var correctAnswer = questionList[questionCount].answer;
+    var target = this.textContent;
+    console.log(correctAnswer);
+
+    if (target == correctAnswer) {
+        window.alert("True!");
+        console.log("Timer plus");
+        
+    } else {
+        window.alert("False!");
+        console.log("Timer minus");
+        
+    }
+
+    for (var i = 0; i <= 3; i++) {
+        answers.removeChild(answers.firstChild);
+    }
+
+    questionCount++;
+
+    if (questionCount >= questionList.length) {
+        return;
+        // END and go to score screen
+    } else {
+        nextQuestion();
+    }
+
+}
+
+
+
+
+
